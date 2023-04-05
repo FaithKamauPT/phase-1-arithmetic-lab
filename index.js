107433408e063d61b7508f424a450e5543500798
FaithKamau <faith.kamau@student.moringaschool.com>
2023-04-05 15:28:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-
 // Write your code here
+const chai = 'chai';
+const expect = chai.expect;
+
+function multiply ();{
+num1 * num2 ;function ans();{
+num1.isInteger(); 
+num2.isInteger();
+multiply = 62;
+};
+
+};
+
+function random();{
+    if(!(random.isInteger>0)){'True';}
+    else
+    {
+        'Not equal';
+    };
+};
+
+function mod();{
+    num3 % num4; function ans();{
+        num3.isInteger();
+        num4.isInteger();
+        mod == 4;
+    };
+};
+
+function max();{
+    max.Mathmax();function ans();{
+        max = 20;
+    };
+};
